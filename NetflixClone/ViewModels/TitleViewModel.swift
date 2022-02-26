3c746eaf6473a1a56d885fca12bdb916841009b8
Dowirak228 <90527580+Dowirak228@users.noreply.github.com>
2022-02-26 19:00:06 +0500

    Upcoming controller

---
+++ NetflixClone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by NODIR KARIMOV on 26/02/22.
+//
+
+import UIKit
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
